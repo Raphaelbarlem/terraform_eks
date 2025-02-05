866d55f905a9a96eb42ec419abd8a338ca40d741
Raphael Augusto Barbosa Lemes <rlemes@uolinc.com>
2025-02-05 14:37:16 -0300

    feat: private

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "cidr_block" {
   type        = string
   description = "Networking CIDR block to be used for the VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "project name to be used to name te resources (tag name) "
 
 }
